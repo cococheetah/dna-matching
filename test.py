0dafcdb83738c232de184f7ac01a841981d31c0b
cococheetah <elizabeth.cococheetah@gmail.com>
2025-04-03 22:47:33 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+import unittest
+from main import read, longest_consecutive_str, create_list, main
+
+class TestDNAMatching(unittest.TestCase):
+    
+    def test_read(self):
+        data = read("test.csv")
+        lists = [["Names","AGATG","AATG","TAT"], ["Elle","5","2","8"],["Amani","3","7","4"],["Blair","6","1","5"],["Ashley","3","7","1"]]
+        self.assertEqual(data, lists)
+
+    def test_longest_consecutive_str(self):
+        result = longest_consecutive_str("AATAATTAATTAATTAAATGAATGAATGAATGAATGAATGTATTATCATTAAATGT", "AATG")
+        self.assertEqual(result, 6)
+
+    def test_create_list(self):
+        database = [["Names", "AGATG", "AATG"], ["Elle", "3", "5"]]
+        sequence = "AGATGAGATGAATGAATGAATGAATG"
+        result = create_list(database, sequence)
+        self.assertEqual(result, [2, 4])
+
+if __name__ == "__main__":
+    unittest.main()
